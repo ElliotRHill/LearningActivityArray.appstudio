d63f0022739e9682033f6f98c6c78dd6ac29c7f9
Elliot R Hill <erh81010@creighton.edu>
2021-02-24 16:30:18 -0600

    Pets Form
    Added Pets Form and Arrays

---
+++ forms/Pets/Pets.js
@@ -0,0 +1,8 @@
+let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+let withSal = petNames
+let sal = "Sal"
+petNames.push(sal)
+withSal.push(sal)
+console.log(`The third pet is named ${petNames[2]}`)
+console.log(`The array has ${petNames.length} names in it.`)
+petNames.pop()
